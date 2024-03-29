8e01e5dbadbbd3e5f222852d579b4635acc4d580
niti-di1977 <60072970+niti-di1977@users.noreply.github.com>
2024-03-29 14:56:11 +0530

    Add files via upload

---
+++ demojen/src/main/java/com/example/demojen/DemojenApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemojenApplication.class, args);
+	}
+
+}
---
+++ demojen/src/test/java/com/example/demojen/DemojenApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.demojen;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemojenApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+		assertTrue(true);
+	}
+
+}
